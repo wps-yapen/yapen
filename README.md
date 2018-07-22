5638144983f4125f1deb3befa16c0a85e53dba3e
pmb <gambler1541@gmail.com>
2018-07-22 15:33:07 +0900

    Custom UserModel

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # yapen
+
+## Requirements
+
+- Python (3.6)
+- Django (2.x)
+
+### Secrets
+
+#### `.sercets/base.json`
+
+```json
+{
+  "SECRET_KEY": "<Django secret key>"
+}
+```
+
+## Installation
+```
+pyenv install
+```
+
+## Running
+
+```
+# Move project`s directory
+- pipenv install
+- pipenv shell
+- cd app
+- export DjANGO_SETTINGS_MODULE=config.settings.local
+- `./manage.py runserver`
+```
---
+++ app/config/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = '/static/'
