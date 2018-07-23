25cc776a1313f5c21e81899491ea6364d35dc7cc
pmb <gambler1541@gmail.com>
2018-07-23 11:47:20 +0900

    Dockerfile FROM을 개인저장소로 지정

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,6 @@
-FROM            python:3.6.5-slim
+FROM            gambler1541/yapen-docker-hub:base
 MAINTAINER      gambler1541@gmail.com
 
-# pip install uwsgi
-RUN             apt -y update && apt -y dist-upgrade
-RUN             apt -y install build-essential
-RUN             apt -y install nginx supervisor
-
-# 로컬의 requirements.txt 파일을 /srv에 복사 후 pip install 실행
-COPY            ./requirements.txt  /srv/
-RUN             pip install -r /srv/requirements.txt
 
 # production
 ENV             BUILD_MODE              production
--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,46 +1,88 @@
+import sys
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = False
-
-
 secrets = json.load(open(os.path.join(SECRET_DIR, 'production.json')))
 
-ALLOWED_HOSTS = [
-    'localhost',
-]
+RUNSERVER = 'runserver' in sys.argv
+ALLOWED_HOSTS = secrets['ALLOWED_HOSTS']
 
+if RUNSERVER:
+    DEBUG = True
+    ALLOWED_HOSTS =[
+        'localhost',
+        '127.0.0.1',
+    ]
 
-DEFAULT_FILE_STORAGE = 'config.storages.S3DefaultStorage'
-STATICFILES_STORAGE = 'config.storages.S3StaticStorage'
-
-
-AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
-AWS_DEFAULT_ACL = secrets['AWS_DEFAULT_ACL']
-AWS_S3_REGION_NAME = secrets['AWS_S3_REGION_NAME']
-AWS_S3_SIGNATURE_VERSION = secrets['AWS_S3_SIGNATURE_VERSION']
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
-
+# wsgi
+WSGI_APPLICATION = 'config.wsgi.production.application'
 
 INSTALLED_APPS += [
     'storages',
 ]
 
+# AWS
+AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
 
+DEFAULT_FILE_STORAGE = 'config.storages.S3DefaultStorage'
+# MEDIA
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
 
-WSGI_APPLICATION = 'config.wsgi.production.application'
+# Databaseses
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databa
 
 DATABASES = secrets['DATABASES']
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
+# log
+LOG_DIR = '/var/log/django'
+# if not os.path.exists(LOG_DIR):
+#     LOG_DIR = os.path.join(ROOT_DIR, '.log')
+#     if not os.path.exists(LOG_DIR):
+#         os.mkdir(LOG_DIR)
+
+if not os.path.exists(LOG_DIR):
+    LOG_DIR = os.path.join(ROOT_DIR,'.log')
+    os.makedirs(LOG_DIR, exist_ok=True)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            'format': '[%(asctime)s] %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'file_error': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'ERROR',
+            'formatter': 'django.server',
+            'backupCount': 10,
+            'filename': os.path.join(LOG_DIR, 'error.log'),
+            'maxBytes': 10485760,
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file_error'],
+            'level': 'INFO',
+            'propagate': True,
+        }
+    }
+}
