7a7ee15239a9a0ae40210ae43edf9c7910282978
pmb <gambler1541@gmail.com>
2018-08-06 13:05:45 +0900

    signup, login, userdetail

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import SignUp, UserActivate, LoginApiView
+from .views import SignUp, UserActivate, UserDetailView
 
 app_name = 'members'
+
 urlpatterns = [
     path('signup/', SignUp.as_view(), name='signup'),
-    path('login/', LoginApiView.as_view(), name='login'),
     path('activate/<str:uidb64>/<str:token>', UserActivate.as_view(), name='activate'),
+    path('detail/', UserDetailView.as_view(), name='detail'),
 ]
