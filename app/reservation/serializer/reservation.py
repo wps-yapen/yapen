d268a14e435623962c719bb61d5c05fa22545b53
sangmin <sang93423@gmail.com>
2018-08-08 18:32:12 +0900

    reservation app추가 날짜로 필터링 하기전 방에 대한 정보들만 우선 출력

---
+++ app/reservation/serializer/reservation.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from location.models import Room
+from location.serializer.room import RoomBaseSerializer
+from reservation.models import Reservation
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+		class Meta:
+			model = Reservation
+			fields = (
+				'checkin_date',
+				'checkout_date',
+			)
+
+
+class RoomReservationSerializer(RoomBaseSerializer):
+	reservations = ReservationSerializer(many=True, read_only=True)
+	class Meta(RoomBaseSerializer.Meta):
+		fields = RoomBaseSerializer.Meta.fields + (
+			'reservations',
+			'extra_charge_adult',
+			'extra_charge_child',
+			'extra_charge_baby',
+		)
