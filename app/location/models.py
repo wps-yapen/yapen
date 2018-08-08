771ed1ecda7fc32c5784140b9c93e0f8d3e7e8d4
pmb <gambler1541@gmail.com>
2018-08-08 13:25:50 +0900

    location, members, crawler

--- app/location/models.py
+++ app/location/models.py
@@ -22,20 +22,20 @@ __all__ = (
 # Create your models here.
 class Pension(models.Model):
     # 세부지역 메이페이지에서 크롤링할때 체우는 속성
-    name = models.CharField(max_length=100) # '팬션이름'
+    name = models.TextField(blank=True) # '팬션이름'
     pension_image_thumbnail = models.ImageField(upload_to='pension', blank=True) # '팬션 이미지' # 해커톤때 photo # MEDIA_ROOT의세부경로가 upload_to에 들어감.
     lowest_price = models.IntegerField(default=0, blank=True) # '최저가'  # 해커톤때 price
     ypidx = models.IntegerField(default=0, blank=True) # '팬션별 고유번호'
-    location = models.CharField(max_length=100, blank=True) #지역
-    sub_location = models.CharField(max_length=100, blank=True)# 세부지역
-    sub_location_no = models.CharField(max_length=100, blank=True)# 세부지역 번호
+    location = models.TextField(blank=True) #지역
+    sub_location = models.TextField(blank=True)# 세부지역
+    sub_location_no = models.TextField(blank=True)# 세부지역 번호
     discount_rate = models.IntegerField(default=0,blank=True)# 할인률
 
     # pension-detail 페이지에서 크롤링해서 채우는 속성
-    address = models.TextField(max_length=200, blank=True) # '주소'
-    check_in = models.CharField(max_length=100,blank=True) # '입실시간'
-    check_out = models.CharField(max_length=100,blank=True) # '퇴실시간'
-    pickup = models.CharField(max_length=100,blank=True) # 픽업 여부
+    address = models.TextField(blank=True) # '주소'
+    check_in = models.TextField(blank=True) # '입실시간'
+    check_out = models.TextField(blank=True) # '퇴실시간'
+    pickup = models.TextField(blank=True) # 픽업 여부
     room_num = models.IntegerField(default=0, blank=True)# '객실 수'  # 해커톤때 room
     info = models.TextField(blank=True)# '공지사항'
     theme = models.TextField(blank=True)# '테마' ----------> 이후에 view에서 json->list로 바꿔서 잘써야함.
@@ -59,18 +59,18 @@ class Pension(models.Model):
 class Room(models.Model):
     pension = models.ForeignKey(Pension, related_name='rooms', on_delete=models.CASCADE)
 
-    name = models.CharField(max_length=100) # '객실 명'
-    structure = models.CharField(max_length=100,blank=True) # '객실구조'
+    name = models.TextField(blank=True) # '객실 명'
+    structure = models.TextField(blank=True) # '객실구조'
     equipments = models.TextField(blank=True) # '구비시설'
     info = models.TextField(blank=True) # '객실설명'
-    size = models.CharField(max_length=100,blank=True) # '크기'
+    size = models.TextField(blank=True) # '크기'
     normal_num_poeple = models.IntegerField(default=0,blank=True) # '기준인원'
     max_num_people = models.IntegerField(default=0,blank=True) # '최대인원'
     price = models.IntegerField(default=0,blank=True) # '가격'
 
     # 추가요금안내
     # '인원추가 불가능' 시에는 여기 그대로 넣고 if문으로 아예 아래는 값 대입하지 말자.
-    extra_charge_head = models.CharField( max_length=100,blank=True)
+    extra_charge_head = models.TextField(blank=True)
     extra_charge_adult = models.IntegerField(default=0,blank=True)
     extra_charge_child = models.IntegerField(default=0,blank=True)
     extra_charge_baby = models.IntegerField(default=0,blank=True)
@@ -78,12 +78,12 @@ class Room(models.Model):
 
 class PensionImage(models.Model):
     pension = models.ForeignKey(Pension,related_name='pensionimages', on_delete=models.CASCADE,)
-    pension_image = models.ImageField(upload_to='pension', blank=True) # '팬션 이미지'
+    pension_image = models.ImageField(upload_to='pension', blank=True, max_length=1000) # '팬션 이미지'
 
 
 class RoomImage(models.Model):
     room= models.ForeignKey(Room,related_name='roomimages',on_delete=models.CASCADE,)
-    room_image = models.ImageField(upload_to='room', blank=True) # '방 이미지'
+    room_image = models.ImageField(upload_to='room', blank=True, max_length=1000) # '방 이미지'
 
 # 반대쪽 입장에서 이것이 뭔지 생각해서 related_name 정한다.
 class Reservation(models.Model):
