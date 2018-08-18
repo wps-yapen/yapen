34a28a443c9ab058f341d6a9a23fb00d7f16227d
nadcdc <nadcdc4@gmail.com>
2018-08-18 23:08:23 +0900

    buttonsearch 필터 apiview로 다시 작성함. pension을 serializer헤서 보여주는 방식. reservation 해당날짜에 있으면 거르기 까지함.

--- app/search/serializer.py
+++ app/search/serializer.py
@@ -1,16 +1,22 @@
 # 버튼 검색 결과로 보여줄 room의 filed만 가진 serailizer(기존 base+ pension, pension의 사진, 이름,pk)
-from location.serializer.pension import PensionNameSerializer
+from location.serializer.pension import PensionNameSerializer, PensionImageSerializer
 from location.serializer.room import RoomBaseSerializer
 
 
 # 버튼 검색 결과로 보여줄 room의 field로 들어갈 pensio객체 serialize 하기위한 것
 class PensionButtonSerachResultSerializer(PensionNameSerializer):
+    rooms = RoomBaseSerializer(many=True, read_only=True)
+    pensionimages = PensionImageSerializer(many=True, read_only=True)
+
     class Meta(PensionNameSerializer.Meta):
         fields = PensionNameSerializer.Meta.fields + (
             'pensionimages',
+            'rooms',
         )
 
 
+
+######@@@@@@@@@@@@@@@@@ 이하 임시저장. 참고만 해라. 나중에 지우도록 해라.
 class RoomButtonSearchResultSerializer(RoomBaseSerializer):
     pension = PensionButtonSerachResultSerializer(read_only=True)
 
@@ -18,3 +24,6 @@ class RoomButtonSearchResultSerializer(RoomBaseSerializer):
         fields = RoomBaseSerializer.Meta.fields +(
             'pension',
         )
+
+
+
