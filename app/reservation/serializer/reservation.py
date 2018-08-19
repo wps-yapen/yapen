ecbf4aed95ce2f82ba4ce7545133d2d2a3954bab
nadcdc <nadcdc4@gmail.com>
2018-08-19 20:15:37 +0900

    PensionReservationSerializer 만들어서 기존에 방 각각의 예약 status 확인 하던 페이지에서 Pension을 상위에 놓고 필요 filed들 보여주고 하위에 room들을 전부 가져오도록 함(기존의 status save해주는 과정은 그대로 썼음.)

--- app/reservation/serializer/reservation.py
+++ app/reservation/serializer/reservation.py
@@ -1,6 +1,9 @@
+import re
+
 from rest_framework import serializers
 
 from location.models import Room
+from location.serializer.pension import PensionNameSerializer
 from location.serializer.room import RoomBaseSerializer
 from reservation.models import Reservation
 
@@ -16,7 +19,7 @@ class ReservationSerializer(serializers.ModelSerializer):
 
 
 class RoomReservationSerializer(RoomBaseSerializer):
-	reservations = ReservationSerializer(many=True, read_only=True)
+	# reservations = ReservationSerializer(many=True, read_only=True)
 	class Meta(RoomBaseSerializer.Meta):
 		fields = RoomBaseSerializer.Meta.fields + (
 			'pk',
@@ -24,5 +27,25 @@ class RoomReservationSerializer(RoomBaseSerializer):
 			'extra_charge_child',
 			'extra_charge_baby',
 			'status',
-			'reservations',
+			# 'reservations',
+		)
+
+class PensionReservationSerializer(PensionNameSerializer):
+
+	rooms = RoomReservationSerializer(many=True, read_only=True)
+
+	def to_representation(self, instance):
+		"""Convert `username` to lowercase."""
+		ret = super().to_representation(instance)
+
+		address =ret['address']
+		result = re.findall('지번 : (.*) ', address)
+		ret['address'] = result
+		return ret
+
+
+	class Meta(PensionNameSerializer.Meta):
+		fields = PensionNameSerializer.Meta.fields + (
+			'address',
+			'rooms'
 		)
