6c0f65a52899bd2d9c6ee80f9fc1a74962cb5c0c
nadcdc <nadcdc4@gmail.com>
2018-08-21 01:04:45 +0900

    IOS 쪽에서 pension main list, pensoin locatoin list 맨처음 방식대로 location, sublocation 이 상위에 나오는 방식으로 바꿔달래서 기존에것 두고 추가만 했음.

--- app/location/urls.py
+++ app/location/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from django.urls import path
 
-from .views import PensionLocationNamesList, PensionDetail, PensionSubLocationList, PensionMainList
+from .views import PensionLocationNamesList, PensionDetail, PensionSubLocationList, PensionMainList, IosPensionMainList, \
+    IosPensionSubLocationList
 
 urlpatterns = [
     path('',
@@ -10,10 +11,20 @@ urlpatterns = [
     path('location-name/',
          PensionLocationNamesList.as_view(),
          name='PensionLocationsList'),
+
+    # IOS 요청으로 예비로 만들어놈.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    path('ios/', IosPensionMainList.as_view(),
+         name='IosPensionMainList'),
+    path('ios/<str:sub_location_no>/', IosPensionSubLocationList.as_view(),
+         name='IosPensionLocationsList'),
+    # IOS 요청으로 예비로 만들어놈.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
     path('<str:sub_location_no>/',
          PensionSubLocationList.as_view(),
          name='PensionSubLocationList'),
     path('<str:sub_location_no>/<int:pk>/',
          PensionDetail.as_view(),
          name='PensionList'),
+
 ]
--- app/location/views.py
+++ app/location/views.py
@@ -5,7 +5,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializer.location import LocationSerializer, SubLocationSerializer
+from .serializer.location import LocationSerializer, SubLocationSerializer, IosLocationSerializer, \
+    IosSubLocationSerializer
 from .serializer.pension import PensionDetailSerializer, PensionListSerializer
 from .models import Pension, Location, SubLocation, Room
 
@@ -63,4 +64,40 @@ class PensionDetail(APIView):
         pension = self.get_object(pk=pk)
         serializer = PensionDetailSerializer(pension)
         return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# IOS 요청으로 예비로 만들어놈.
+class IosPensionMainList(APIView):
+
+    def get(self,request,format=None):
+        locations = Location.objects.all()
+        serializer = IosLocationSerializer(locations, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+# IOS 요청으로 예비로 만들어놈.
+class IosPensionSubLocationList(APIView):
+
+    def get(self,request,sub_location_no,format=None):
+        sub_location = SubLocation.objects.filter(sub_location_no=sub_location_no)
+        serializer = IosSubLocationSerializer(sub_location, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+
+
 
