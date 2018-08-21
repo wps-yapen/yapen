eea65a38d9290e807e16c007b0d0fc75ba26fd2d
pmb <gambler1541@gmail.com>
2018-08-21 13:13:07 +0900

    apis 분류

--- app/location/urls.py
+++ app/location/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
 
-from .views import PensionLocationNamesList, PensionDetail, PensionSubLocationList, PensionMainList
+from .apis import PensionLocationNamesList, PensionDetail, PensionSubLocationList, PensionMainList
 
 urlpatterns = [
     path('',
--- app/members/apis/__init__.py
+++ app/members/apis/__init__.py
@@ -8,7 +8,6 @@ from rest_framework.authtoken.models import Token
 from rest_framework.compat import authenticate
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
-
 from rest_framework.views import APIView
 
 from members.serializer import UserCreateSerializer, UserDetailSerializer, UserPasswordChange, FacebookUserSerializer
@@ -38,19 +37,19 @@ class UserActivate(APIView):
         except(TypeError, ValueError, OverflowError, User.DoseNotExist):
             user = None
 
-
         try:
             if user is not None and account_activation_token.check_token(user, token):
                 user.is_active = True
                 user.save()
                 return Response(user.email + '계정이 활성화 되었습니다. ', status=status.HTTP_200_OK)
             else:
-                return Response('만료된 링크입니다.', status = status.HTTP_400_BAD_REQUEST)
+                return Response('만료된 링크입니다.', status=status.HTTP_400_BAD_REQUEST)
 
         except Exception as e:
             print(traceback.format.exc())
 
-#login
+
+# login
 class AuthToken(APIView):
     def post(self, request):
         username = request.data.get('username')
@@ -58,10 +57,10 @@ class AuthToken(APIView):
         user = authenticate(username=username, password=password)
 
         if user:
-            token, _= Token.objects.get_or_create(user=user)
+            token, _ = Token.objects.get_or_create(user=user)
 
             data = {
-                'token' : token.key,
+                'token': token.key,
             }
             return Response(data, status=status.HTTP_200_OK)
         else:
@@ -72,13 +71,15 @@ class UserDetailView(APIView):
     permission_classes = (
         permissions.IsAuthenticated,
     )
+
     def get(self, request):
         serializer = UserDetailSerializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
 class UserChangePassword(APIView):
     permission_classes = (
-    permissions.IsAuthenticated,
+        permissions.IsAuthenticated,
     )
 
     def patch(self, request, *args, **kwargs):
@@ -87,10 +88,11 @@ class UserChangePassword(APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response('비밀번호가 변경되었습니다.',status=status.HTTP_204_NO_CONTENT)
+            return Response('비밀번호가 변경되었습니다.', status=status.HTTP_204_NO_CONTENT)
 
         else:
             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserDetailView(APIView):
     permission_classes = (
@@ -101,10 +103,9 @@ class UserDetailView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-
 class UserChangePassword(APIView):
     permission_classes = (
-    permissions.IsAuthenticated,
+        permissions.IsAuthenticated,
     )
 
     def patch(self, request, *args, **kwargs):
@@ -128,7 +129,6 @@ class Deletetoken(APIView):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-
 class FacebookLogin(APIView):
     def post(self, request):
         facebook_id = request.data.get('facebook_id')
@@ -137,16 +137,14 @@ class FacebookLogin(APIView):
         user, __ = User.objects.get_or_create(
             username=facebook_id,
             defaults={
-                'last_name':last_name,
-                'first_name':first_name,
+                'last_name': last_name,
+                'first_name': first_name,
             }
         )
 
         token, __ = Token.objects.get_or_create(user=user)
         data = {
-            'token':token.key,
-            'user':FacebookUserSerializer(user).data
+            'token': token.key,
+            'user': FacebookUserSerializer(user).data
         }
         return Response(data)
-
-
