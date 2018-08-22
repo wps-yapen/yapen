f21e16dc7152dc1909e97e0548f9167d8f812a02
pmb <gambler1541@gmail.com>
2018-08-22 15:46:30 +0900

    Front deploy 실패

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -55,18 +55,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Front
+CORS_ORIGIN_WHITELIST = (
+    'localhost:4200',
+
+)
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
